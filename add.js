c2f29a52d69632a645db466097d77beda88fd525
ALsmadiahmad93 <>
2021-05-27 12:33:23 +0000

    i added a javascrept file

---
+++ add.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var userName =prompt(" what's your name ");
+
+
+
+
+
+
+//console.log(userName);
+
+if(userName === 'ahmadsmadi'){
+  alert('welcome boss');
+}
+else if (userName === 'mohammed'){
+  alert('what are you doing');
+}
+
+ alert('you have to chose between ahmadsmadi or mohammed next time ')
