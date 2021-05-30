95f9950c4e9afc65832e7dca275b489905125d4e
ALsmadiahmad93 <>
2021-05-30 19:06:21 +0000

    i edited the add.js and added some function

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-var userName = "pet";
+/*var userName = "pet";
 
 var userName = prompt("What is your pet name ?");
 alert("welcome to pet world " + userName );
@@ -18,9 +18,18 @@ var petAge = prompt("How old is your pet");{
   alert("please let it happy");
 }
 
+*/
 
+alert("welcome to the dog world");
+let userInput = prompt("how many dogs you have");
+while (userInput !=3){
+  userInput = prompt("lets try again ! how many dogs you have? hint:choose 3!!");
+  if(userInput==3)break;
+}
 
+for(var i=0;i<3;i++)
+{    document.write("<img src='https://www.dogstrust.org.uk/help-advice/_images/164742v800_puppy-1.jpg'>");
+}
 
-  
 
 
