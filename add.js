88f4b87a00e4ea65739e6b06ffb1d6d908775de9
ALsmadiahmad93 <>
2021-05-30 13:27:39 +0000

    i edited add.js file

--- add.js
+++ add.js
@@ -1,19 +1,25 @@
-'use strict';
+var userName = "pet";
 
-var userName =prompt(" what's your name ");
+var userName = prompt("What is your pet name ?");
+alert("welcome to pet world " + userName );
+
+var petColor = prompt("What is your pet color  ?");
+
+if( petColor === "white") { 
+  alert (" Welcome to white world ");
+  } else if (petColor === "black"){
+    alert("welcome to black world");
+  }else {
+    document.write( '<img src="https://cf.ltkcdn.net/dogs/images/orig/264655-1600x1030-couple-white-dog.jpg" width="250px" alt="whit dog" />'  );
+  }
 
 
 
 
 
 
-//console.log(userName);
 
-if(userName === 'ahmadsmadi'){
-  alert('welcome boss');
-}
-else if (userName === 'mohammed'){
-  alert('what are you doing');
-}
 
- alert('you have to chose between ahmadsmadi or mohammed next time ')
+  
+
+
