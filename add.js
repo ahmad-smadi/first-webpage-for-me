647283a2491e7725294040141094316f49c74efa
ALsmadiahmad93 <>
2021-05-31 12:30:32 +0000

    i added a stayle.css file

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-var userName = "pet";
+/*var userName = "pet";
 
 var userName = prompt("What is your pet name ?");
 alert("welcome to pet world " + userName );
@@ -17,9 +17,9 @@ if( petColor === "white") {
 var petAge = prompt("How old is your pet");{
   alert("please let it happy");
 }
+*/
 
-
-alert("welcome to the dog world");
+/* alert("welcome to the dog world");
 let userInput = prompt("how many dogs you have");
 while (userInput !=3){
   userInput = prompt("lets try again ! how many dogs you have? hint:choose 3!!");
@@ -30,5 +30,6 @@ for(var i=0;i<3;i++)
 {    document.write("<img src='https://www.dogstrust.org.uk/help-advice/_images/164742v800_puppy-1.jpg'>");
 }
 
+*/
 
 
